b9cbfe005f1c593517c17a424ecd34932071fcff
bahamianseafoodconnection-collab <bahamianseafoodconnection@gmail.com>
2026-04-18 14:05:48 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Page() {
-return (
-<main>
-<h1>BSC Dashboard</h1>
-<p>Deployment is working</p>
-</main>
-)
+  return (
+    <main>
+      <h1>BSC Dashboard</h1>
+      <p>Deployment is working</p>
+    </main>
+  )
 }
