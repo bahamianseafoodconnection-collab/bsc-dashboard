8ac8d601fab97fb3451ec8e0aab72ee46401c055
bahamianseafoodconnection-collab <bahamianseafoodconnection@gmail.com>
2026-04-18 09:26:43 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,20 @@
-import './globals.css'
+// app/layout.tsx
+import type { Metadata } from 'next';
+import './globals.css';
 
-export default function RootLayout({ children }) {
+export const metadata: Metadata = {
+title: 'BSC Dashboard',
+description: 'Bahamian Seafood Connection Dashboard',
+};
+
+export default function RootLayout({
+children,
+}: {
+children: React.ReactNode;
+}) {
 return (
-<html>
+<html lang="en">
 <body>{children}</body>
 </html>
-)
+);
 }
