8779fc5641079e0a690848c2094522140965a532
bahamianseafoodconnection-collab <bahamianseafoodconnection@gmail.com>
2026-04-18 10:05:21 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,9 @@
-// app/layout.tsx
-import type { Metadata } from 'next';
-import './globals.css';
+import './globals.css'
 
-export const metadata: Metadata = {
-title: 'BSC Dashboard | Bahamian Seafood Connection',
-description: 'Dashboard for Bahamian Seafood Connection',
-};
-
-export default function RootLayout({
-children,
-}: {
-children: React.ReactNode; // This is fine if React is available via Next.js types
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 return (
 <html lang="en">
-<body>
-{children}
-</body>
+<body>{children}</body>
 </html>
-);
+)
 }
