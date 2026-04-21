0edb0e20be729da4b138284c2dad7f4828393522
bahamianseafoodconnection-collab <bahamianseafoodconnection@gmail.com>
2026-04-21 12:00:38 -0400

    Create page.tsx

---
+++ app/bills/page.tsx
@@ -0,0 +1,12 @@
+export default function BillsPage() {
+  return (
+    <>
+      <h2 className="page-title">Bills</h2>
+
+      <div className="placeholder-box">
+        <p>Bills screen</p>
+        <p>This is where bill payment form and bill history will go next.</p>
+      </div>
+    </>
+  )
+}
