b35ded7d8a9f11a9daf0ad278cbce0a8bf3e9b2a
bahamianseafoodconnection-collab <bahamianseafoodconnection@gmail.com>
2026-04-23 11:47:47 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-"use client"
+"use client";
 
-import SupplierChatModule from "../components/suppliers/SupplierChatModule"
+import SupplierChatModule from "@/components/suppliers/SupplierChatModule";
 
 export default function Page() {
-  return (
-    <main className="min-h-screen bg-white p-4">
-      <SupplierChatModule />
-    </main>
-  )
+  return <SupplierChatModule />;
 }
