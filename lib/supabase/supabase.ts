dc9130b4f9d4b41eb0f7b3d9dc13e434a2cc7e1c
bahamianseafoodconnection-collab <bahamianseafoodconnection@gmail.com>
2026-04-24 20:31:38 -0400

    Delete supabase.ts

--- lib/supabase/supabase.ts
+++
@@ -1,7 +0,0 @@
-// File: lib/supabase.ts
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://auqjjrisivhfmpleusyt.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1cWpqcmlzaXZoZm1wbGV1c3l0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MTk4NDcsImV4cCI6MjA5MTM5NTg0N30.gukwxBD4tFRVWMiA8_fauiV2JdEyvXMYJjzLcZiZpCg";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
